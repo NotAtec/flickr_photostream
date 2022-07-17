23a7863e638edc39444eb374c0fa0404ce5cc38a
Jeroen B <jeroenb04@live.com>
2022-07-17 17:02:56 +0200

    Created form & photostream response

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,14 @@
 class StaticPagesController < ApplicationController
 
   def home
+    flickr = Flickr.new
+
+    @photos = if params[:query][:user_id].blank?
+                flickr.photos.getRecent
+              else
+                flickr.photos.search(user_id: params[:query][:user_id])
+              end
+  rescue StandardError => e
+    flash[:alert] = "#{e.class}: #{e.message}. Try Again."
   end
 end
